9a74ecb4e39385eee117bc8345385664e0ff7201
Jonaka <jonakactfs@gmail.com>
2023-04-14 16:09:25 +0200

    versuche in task

--- hackfest/task.py
+++ hackfest/task.py
@@ -1,9 +1,13 @@
 from week1.cipher import XORCipher
-from secret import FLAG
 
 key = b'Hurra!'
-cipher = XORCipher(key)
-ciphertext = cipher.encrypt(FLAG)
-print(ciphertext.hex())
+ciphertext = b'0b272b092e6a173220372075173b3d253e6d0d21212d26641c2a263d3e760727390f'
+cipher = XORCipher(ciphertext)
+klartext = cipher.encrypt(key)
+print(klartext)
+print(klartext.hex())
+print(klartext.decode())
+print(klartext.hex().encode())
+print(klartext.decode().encode())
 # Output:
 # 0b272b092e6a173220372075173b3d253e6d0d21212d26641c2a263d3e760727390f
