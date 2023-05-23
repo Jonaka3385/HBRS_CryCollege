973e9f0a208341f88c11ee8dfb14b08dcf538929
Jonaka <jonakactfs@gmail.com>
2023-05-23 11:24:33 +0200

    week5 genauso scheisse wie alle anderen

--- hackfest/cry_college_week5_task02_155_server.py
+++ hackfest/cry_college_week5_task02_155_server.py
@@ -5,11 +5,14 @@ import ecdsa
 if __name__ == '__main__':
     msg = 'Yes, I did east the last cookie!'
 
+    gg = ecdsa
     path_priv = "/Users/jonas/Documents/JetBrains_Projects/PyCharm/CryCollege/hackfest/prime256v1-key.pem"
     key = open(path_priv).read()
-    sk = ecdsa.SigningKey.from_pem(key)
+    sk = gg.SigningKey.from_pem(key)
     path_pub = "/Users/jonas/Documents/JetBrains_Projects/PyCharm/CryCollege/hackfest/mypubkey.pem"
     pubkey = open(path_pub).read()
-    vk = ecdsa.VerifyingKey.from_pem(pubkey)
+    vk = gg.VerifyingKey.from_pem(pubkey)
 
-    send_receive('hackfest.redrocket.club', 21555, '')
+    signed = b'MEUCIDohwrOqfygcgP68/OIcZnmeSMVifM8079JKSEO4u9XxAiEA6eDMWIDae79Ah//6DcfR9jA4sYn7HKiJUa62wZKJ92A='
+
+    send_receive('hackfest.redrocket.club', 21555, signed)
